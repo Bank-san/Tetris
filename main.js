7ebea27519cbad910f17a56d508b352888bbdac2
bank-san <taichi.a@itnav.co.jp>
2024-02-22 10:23:48 +0900

    add:selecter, none

--- main.js
+++ main.js
@@ -336,3 +336,19 @@ upButton.addEventListener("touchmove", preventZoom);
 downButton.addEventListener("touchmove", preventZoom);
 rotateButton.addEventListener("touchmove", preventZoom);
 holdButton.addEventListener("touchmove", preventZoom);
+
+$(function () {
+  var audioBtn = $(".audio_button"),
+    audioWrap = $(".audio_wrap"),
+    audio = document.getElementById("audio");
+
+  audioBtn.on("click", function () {
+    if (audioWrap.hasClass("play")) {
+      audio.pause();
+      audioWrap.removeClass("play");
+    } else {
+      audio.play();
+      audioWrap.addClass("play");
+    }
+  });
+});
